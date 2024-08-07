62d043ffce41391b33687025d9783c7a4c035a0e
Rohit685 <mittalr685@gmail.com>
2024-08-07 12:11:16 -0500

    feat: started on CreateForecast method

--- Models/Forecast.cs
+++ Models/Forecast.cs
@@ -5,6 +5,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DynamicWeather.Enums;
+using DynamicWeather.Extensions;
 using DynamicWeather.Helpers;
 using Rage;
 using Rage.Native;
@@ -17,6 +19,41 @@ namespace DynamicWeather
         private int currWeatherIndex;
         internal Weather CurrentWeather => WeatherList[currWeatherIndex];
         internal Weather NextWeather => WeatherList[currWeatherIndex + 1] != null ? WeatherList[currWeatherIndex + 1] : WeatherList[0];
+        private List<Text> TextList;
+        private List<Texture> TexturesList;
+        private int timeInterval;
+
+        private static WeatherTypesEnum[] stages = {
+            WeatherTypesEnum.ExtraSunny,
+            WeatherTypesEnum.Clear,
+            WeatherTypesEnum.Foggy,
+            WeatherTypesEnum.Neutral,
+            WeatherTypesEnum.Smog,
+            WeatherTypesEnum.Clouds,
+            WeatherTypesEnum.Overcast,
+            WeatherTypesEnum.Clearing,
+            WeatherTypesEnum.Rain,
+            WeatherTypesEnum.Thunder,
+            WeatherTypesEnum.Clearing,
+            WeatherTypesEnum.Overcast,
+        };
+
+
+        internal Forecast(int interval)
+        {
+            WeatherList = CreateForecast();
+            currWeatherIndex = 0;
+            timeInterval = interval;
+            TextList = new List<Text>();
+            TexturesList = new List<Texture>();
+            for (var index = 0; index < WeatherList.Count; index++)
+            {
+                var weather = WeatherList[index];
+                Text weatherText = new Text(weather.Temperature.ToString(), 20, Color.White);
+                TextList.Add(weatherText);
+                TexturesList.Add(weather.Texture);
+            }
+        }
 
         internal void Process()
         {
@@ -40,22 +77,31 @@ namespace DynamicWeather
             }
         }
 
-        internal static void CreateForecast(int interval)
+        internal static List<Weather> CreateForecast()
         {
-            
+            List<Weather> weatherList = new List<Weather>();
+            Random random = new Random(DateTime.Today.Millisecond);
+            int index = random.Next(stages.Length);
+            weatherList.Add(Weathers.WeatherData[stages[index]]);
+            for (int i = 1; i < 5; i++)
+            {
+                if (random.Next(101) > 50)
+                {
+                    index++;
+                    weatherList.Add(Weathers.WeatherData[stages.GetNext(index)]);
+                }
+                else
+                {
+                    weatherList.Add(Weathers.WeatherData[stages.GetNext(index)]);
+                }
+            }
+            return weatherList;
         }
 
         internal void DrawForecast(Rage.Graphics g)
         {
-            var textList = new List<Text>();
-            var texturesList = new List<Texture>();
-            for (var index = 0; index < WeatherList.Count; index++)
-            {
-                var weather = WeatherList[index];
-                Text weatherText = new Text(weather.Temperature.ToString(), 20, Color.White);
-                textList.Add(weatherText);
-                texturesList.Add(weather.Texture);
-            }
+            TextureHelper.DrawTexture(g, TexturesList);
+            TextureHelper.DrawText(g, TextList);
         }
     }
 }
