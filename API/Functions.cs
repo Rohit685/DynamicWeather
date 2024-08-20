1a0b639a6936405144d33e534cc85589ba252da3
Rohit685 <mittalr685@gmail.com>
2024-08-19 21:49:34 -0500

    fix:api bug

--- API/Functions.cs
+++ API/Functions.cs
@@ -26,7 +26,7 @@ public static class Functions
     public static List<Weather> GetCurrentForecast(out int currentWeatherIndex)
     {
         currentWeatherIndex = 0;
-        if (!RealLifeWeatherSync.isRealLifeWeatherSyncRunning) return new List<Weather>();
+        if (RealLifeWeatherSync.isRealLifeWeatherSyncRunning) return new List<Weather>();
         currentWeatherIndex = EntryPoint.currentForecast.currWeatherIndex;
         return EntryPoint.currentForecast.WeatherList;
     }
