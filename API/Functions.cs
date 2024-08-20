fb06cc839be73fbaf35e8c36c2151f40795e8ffa
Rohit685 <mittalr685@gmail.com>
2024-08-19 21:42:47 -0500

    fixed api to acccount for RealLifeWeatherSync

--- API/Functions.cs
+++ API/Functions.cs
@@ -1,23 +1,35 @@
 using System.Collections.Generic;
+using DynamicWeather.Helpers;
 using DynamicWeather.Models;
 
 namespace DynamicWeather.API;
 
 public static class Functions
 {
-    public static void PauseForecast()
+    public static void PauseWeatherSystem()
     {
+        if (RealLifeWeatherSync.isRealLifeWeatherSyncRunning) return;
         EntryPoint.currentForecast.PauseForecast();
     }
     
-    public static void ResumeForecast()
+    public static void ResumeWeatherSystem()
     {
+        if (RealLifeWeatherSync.isRealLifeWeatherSyncRunning)
+        {
+            RealLifeWeatherSync.UpdateWeather();   
+            return;
+        }
         EntryPoint.currentForecast.ResumeForecast();
     }
+    
 
     public static List<Weather> GetCurrentForecast(out int currentWeatherIndex)
     {
+        currentWeatherIndex = 0;
+        if (!RealLifeWeatherSync.isRealLifeWeatherSyncRunning) return new List<Weather>();
         currentWeatherIndex = EntryPoint.currentForecast.currWeatherIndex;
         return EntryPoint.currentForecast.WeatherList;
     }
+    
+    public static bool IsRealLifeWeatherSyncRunning => RealLifeWeatherSync.isRealLifeWeatherSyncRunning;
 }
